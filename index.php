498105d2ad88e573f76e7c3c992d9c256eba1ebb
luizarp <luizarafaela1211@gmail.com>
2022-10-06 01:42:21 -0300

    fix form

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MySite</title>
+  <title>Login - MySite</title>
 
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
@@ -18,12 +17,24 @@
 <body>
   <div id="app">
     <v-app>
-      <v-main id="page">
+      <v-main>
         <v-container>
-          <v-row id="newPage" style="max-width: 100%;height: 100vh" class="d-flex justify-center align-center">
-            <v-col cols="10" md="6" lg="5" class="d-flex flex-column align-center">
-              <h1 class="tracking-in-expand" style="color: #fff">It's yours MySite</h1>
-              <button class="mt-5" onclick="showFlowers()">See flowers</button>
+          <v-row style="max-width: 100%;height: 100vh" class="d-flex justify-center align-center">
+            <v-col cols="10" md="6" lg="5">
+              <v-card>
+                <v-card-title>Faça o login</v-card-title>
+                <v-divider></v-divider>
+                <v-card-text class="d-flex flex-column justify-center">
+                  <form name="login_form" method="POST" action="loged.php">
+                    <label for="user">Usuário</label>
+                    <input type="text" name="user" id="user" required minlength="3">
+                    <label for="password">Senha</label>
+                    <input type="password" name="password" id="password" required minlength="3">
+                    <button type="reset" class="cancel">Cancelar</button>
+                    <button class="save" type="submit">Entrar</button>
+                  </form>
+                </v-card-text>
+              </v-card>
             </v-col>
           </v-row>
         </v-container>
@@ -32,17 +43,12 @@
   </div>
   <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
   <script>
     new Vue({
       el: '#app',
       vuetify: new Vuetify(),
     })
-  </script>
-  <script>
-    function showFlowers() {
-      var element = document.getElementById("page");
-      element.classList.toggle("flowers");
-    }
   </script>
 </body>
 
--- loged.php
+++ loged.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login - MySite</title>
+  <title>MySite</title>
 
   <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css" rel="stylesheet">
@@ -18,24 +17,14 @@
 <body>
   <div id="app">
     <v-app>
-      <v-main>
+      <v-main id="page">
         <v-container>
-          <v-row style="max-width: 100%;height: 100vh" class="d-flex justify-center align-center">
-            <v-col cols="10" md="6" lg="5">
-              <v-card>
-                <v-card-title>Welcome!</v-card-title>
-                <v-card-subtitle>Login</v-card-subtitle>
-                <v-divider></v-divider>
-                <v-card-text class="d-flex flex-column justify-center">
-                  <form action="loged.html">
-                    <label for="name">Name</label>
-                    <input type="text" id="name" required minlength="5">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" required minlength="5">
-                    <button onclick="submitForm">SUBMIT</button>
-                  </form>
-                </v-card-text>
-              </v-card>
+          <v-row id="newPage" style="max-width: 100%;height: 100vh" class="d-flex justify-center align-center">
+            <v-col cols="10" md="6" lg="5" class="d-flex flex-column align-center">
+              <h1 class="tracking-in-expand" style="color: #fff">Bem vindo(a) 
+                <?php print $_POST["user"] ?>
+              </h1>
+              <button class="mt-5 seeFlower" onclick="showFlowers()">See flowers</button>
             </v-col>
           </v-row>
         </v-container>
@@ -44,12 +33,17 @@
   </div>
   <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
   <script>
     new Vue({
       el: '#app',
       vuetify: new Vuetify(),
     })
+  </script>
+  <script>
+    function showFlowers() {
+      var element = document.getElementById("page");
+      element.classList.toggle("flowers");
+    }
   </script>
 </body>
 
